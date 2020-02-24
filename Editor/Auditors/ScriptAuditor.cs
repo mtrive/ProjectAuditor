27a0636800f55b7e3102d44eb50166e69e0449bc
Marco Trivellato <marcot@unity3d.com>
2020-02-24 16:25:02 +0000

    cleanup

--- Editor/Auditors/ScriptAuditor.cs
+++ Editor/Auditors/ScriptAuditor.cs
@@ -102,8 +102,7 @@ namespace Unity.ProjectAuditor.Editor.Auditors
                 return;
             
             var callerNode = new CallTreeNode(caller);
-
-            bool perfCriticalContext = MonoBehaviourAnalysis.IsPerformanceCriticalContext(caller.DeclaringType, caller);
+            var perfCriticalContext = IsPerformanceCriticalContext(caller.DeclaringType, caller);
             
             foreach (var inst in caller.Body.Instructions.Where(i => m_OpCodes.Contains(i.OpCode)))
             {
@@ -184,6 +183,11 @@ namespace Unity.ProjectAuditor.Editor.Auditors
         public static IEnumerable<ProjectIssue> FindScriptIssues(ProjectReport projectReport, string relativePath)
         {
             return projectReport.GetIssues(IssueCategory.ApiCalls).Where(i => i.relativePath.Equals(relativePath));
+        }
+        
+        private static bool IsPerformanceCriticalContext(TypeReference typeReference, MethodDefinition methodDefinition)
+        {
+            return MonoBehaviourAnalysis.IsMonoBehaviour(typeReference) && MonoBehaviourAnalysis.IsMonoBehaviourUpdateMethod(methodDefinition);
         }
     }
 }
--- Editor/Utils/MonoBehaviourAnalysis.cs
+++ Editor/Utils/MonoBehaviourAnalysis.cs
@@ -4,51 +4,46 @@ using UnityEngine;
 
 namespace Unity.ProjectAuditor.Editor.Utils
 {
-internal static class MonoBehaviourAnalysis
-{
-    private static readonly int classNameHashCode = "UnityEngine.MonoBehaviour".GetHashCode();
-
-    private static string[] magicMethodNames = new[]
-        {"Awake", "Start", "OnEnable", "OnDisable", "Update", "LateUpdate", "OnEnable", "FixedUpdate"};
-
-    private static string[] updateMethodNames = new[]
-        {"Update", "LateUpdate", "FixedUpdate"};
-
-    public static bool IsMonoBehaviour(TypeReference typeReference)
+    internal static class MonoBehaviourAnalysis
     {
-        TypeDefinition typeDefinition = null;
-        try
+        private static readonly int classNameHashCode = "UnityEngine.MonoBehaviour".GetHashCode();
+
+        private static string[] magicMethodNames = new[]
+            {"Awake", "Start", "OnEnable", "OnDisable", "Update", "LateUpdate", "OnEnable", "FixedUpdate"};
+
+        private static string[] updateMethodNames = new[]
+            {"Update", "LateUpdate", "FixedUpdate"};
+
+        public static bool IsMonoBehaviour(TypeReference typeReference)
         {
-            typeDefinition = typeReference.Resolve();
-        }
-        catch (AssemblyResolutionException e)
-        {
-            Debug.LogWarning(e);
+            TypeDefinition typeDefinition = null;
+            try
+            {
+                typeDefinition = typeReference.Resolve();
+            }
+            catch (AssemblyResolutionException e)
+            {
+                Debug.LogWarning(e);
+                return false;
+            }
+
+            if (typeDefinition.FullName.GetHashCode() == classNameHashCode && typeDefinition.Module.Name.Equals("UnityEngine.CoreModule.dll"))
+                return true;
+
+            if (typeDefinition.BaseType != null)
+                return IsMonoBehaviour(typeDefinition.BaseType);
+
             return false;
         }
 
-        if (typeDefinition.FullName.GetHashCode() == classNameHashCode && typeDefinition.Module.Name.Equals("UnityEngine.CoreModule.dll"))
-            return true;
-
-        if (typeDefinition.BaseType != null)
-            return IsMonoBehaviour(typeDefinition.BaseType);
-
-        return false;
+        public static bool IsMonoBehaviourMagicMethod(MethodDefinition methodDefinition)
+        {
+            return magicMethodNames.Contains(methodDefinition.Name);
+        }
+        
+        public static bool IsMonoBehaviourUpdateMethod(MethodDefinition methodDefinition)
+        {
+            return updateMethodNames.Contains(methodDefinition.Name);
+        }
     }
-
-    public static bool IsMonoBehaviourMagicMethod(MethodDefinition methodDefinition)
-    {
-        return magicMethodNames.Contains(methodDefinition.Name);
-    }
-    
-    public static bool IsMonoBehaviourUpdateMethod(MethodDefinition methodDefinition)
-    {
-        return updateMethodNames.Contains(methodDefinition.Name);
-    }
-
-    public static bool IsPerformanceCriticalContext(TypeReference typeReference, MethodDefinition methodDefinition)
-    {
-        return IsMonoBehaviour(typeReference) && updateMethodNames.Contains(methodDefinition.Name);
-    }
-}
 }
